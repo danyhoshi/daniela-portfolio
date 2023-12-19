619cd001cfcd120310592279543faba67e664163
dgonzalez777 <1989.daniela@gmail.com>
2023-12-18 21:26:37 -0400

    Add styles for tablet navbar

--- src/app/ui/components/Header.tsx
+++ src/app/ui/components/Header.tsx
@@ -12,12 +12,12 @@ export default function Header() {
     }
     return (
         <header className="flex justify-center items-center h-[4rem] w-full fixed bg-[#1b1a17] z-[999]">
-            <div className={`flex justify-between w-full lg:w-[80%]`}>
+            <div className={`flex justify-between w-full lg:w-[80%] md:justify-center md:items-center md:flex-col lg:justify-between lg:flex-row`}>
                 <div className={`text-[2.2rem] ${anton.className} pl-4`}>
                     <span className={`${inter.className} text-[#ff8303] font-semibold`}>&lt;/</span><span className={`${anton.className} text-[#ff8303]`}>D</span>aniela<span className={`${inter.className} text-[#ff8303] font-bold`}>&gt;</span>
                 </div>
                 
-                <button aria-label="burgermenu" type="button" className="menu-btn block lg:hidden pr-4 z-[999]" onClick={ handleClick }>
+                <button aria-label="burgermenu" type="button" className="menu-btn block lg:hidden md:hidden pr-4 z-[999]" onClick={ handleClick }>
                     <div className={ clsx(``,
                             {
                                 'block': !menu,
@@ -34,12 +34,14 @@ export default function Header() {
                         <svg className="fill-[#f0e3ca]" xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M12.45 38.7 9.3 35.55 20.85 24 9.3 12.5l3.15-3.2L24 20.8 35.55 9.3l3.15 3.2L27.2 24l11.5 11.55-3.15 3.15L24 27.2Z"/></svg>
                     </div>
                 </button>
-                <ul className={clsx(`flex flex-col lg:flex-row justify-center items-center lg:justify-end fixed w-full lg:relative gap-4 text-[1.2rem] ${inconsolata.className} pt-[4rem] pb-[1rem] lg:pt-[1em]  bg-[#00000055] lg:bg-transparent`,
+                <ul className={clsx(`flex flex-col lg:flex-row justify-center items-center lg:justify-end fixed w-full lg:relative gap-4 text-[1.2rem] ${inconsolata.className} pt-[4rem] pb-[1rem] lg:pt-[1em]  bg-[#00000055] lg:bg-transparent md:bg-transparent md:justify-evenly md:pt-[6rem]`,
                 {
                     'block': menu,
                     'hidden': !menu,
                     'lg:flex': menu || !menu,
                     'lg:flex-row': menu || !menu,
+                    'md:flex': menu || !menu,
+                    'md:flex-row': menu || !menu,
                 }
                 )}>
                     <Link href=""><li>About Me</li></Link>
