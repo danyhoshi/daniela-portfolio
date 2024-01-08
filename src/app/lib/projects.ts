6336693ae2bcbc22ac31be730fc48c4e870dad46
dgonzalez777 <1989.daniela@gmail.com>
2024-01-08 00:55:36 -0400

    Add more styles for modal

---
+++ src/app/lib/projects.ts
@@ -0,0 +1,35 @@
+const projects = [
+    {
+        id: 0,
+        title: "Tenzies",
+        tech: "HTML5 - CSS - TypeScript - React",
+        img: "/Tenzies.webp",
+        description: "Let's play Tenzies! Roll until all dice are the same. Click each die to freeze it at its current value between rolls",
+        link: "https://ornate-llama-98e056.netlify.app/"
+    },
+    {
+        id: 1,
+        title: "Space Travelers",
+        tech: "HTML5 - CSS - TypeScript - React - Redux - Bootstrap - Vitest",
+        img: "/spaceTravelers.webp",
+        description: "In this project, we simulate space travel reservations.",
+        link: "https://space-travelers-rust.vercel.app/"
+    },
+    {
+        id: 2,
+        title: "Covid Metrics",
+        tech: "HTML5 - CSS - TypeScript - React - Redux - React-Router - Styled Components - Jest",
+        img: "/covid.webp",
+        description: "COVID Metrics, is a web app using https://covid-19-statistics.p.rapidapi.com/ API, with responsive design you can filter the data by day, and select the country to detail information",
+        link: "https://covid-metrics-alpha.vercel.app/"
+    },
+    {
+        id: 3,
+        title: "Mobile Secure",
+        tech: "HTML5 - CSS - TypeScript - React - Redux - NEXT.JS - Tailwinds CSS",
+        img: "/mobileSecure.webp",
+        description: "In this project, quotes for vehicle insurance policies are simulated.",
+        link: "https://rimac-project-du21.vercel.app/"
+    }
+]
+export default projects;
--- src/app/ui/components/Projects.tsx
+++ src/app/ui/components/Projects.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 import { useState } from 'react';
 import clsx from 'clsx';
 import Link from 'next/link';
+import projects from '@/app/lib/projects';
 export const metadata: Metadata = {
   title: 'Daniela Portfolio',
   description: 'Frontend developer Portfolio',
@@ -17,129 +18,79 @@ export const metadata: Metadata = {
     ]
   }
 }
+interface modal {
+  view: boolean,
+  id: number
+} 
 export default function Projects() {
- const [viewModal, setViewModal]  = useState(false);
-  const showModal = () => {
-    setViewModal(prevState => !prevState)
-    console.log("click")
+ const [viewModal, setViewModal]  = useState<modal>({view: false, id: 0});
+  const showModal = (idP: number) => {
+    setViewModal({
+      view: true,
+      id: idP,
+     })
+    console.log("click: ", idP)
+  }
+  const closeModal = () => {
+    setViewModal(prevState => (
+     {
+      ...prevState,
+      view: false,
+     }
+
+    ))
   }
   return (
     <main className="mt-[8rem] lg:flex lg:justify-center">
       <section className='lg:flex-row lg:max-w-[1024px] works relative'>
-      <div className="work-card z-[1]">
-                <div className="container-project">
-                    <Image 
-                        width="300"
-                        height="300"
-                        src="/Tenzies.webp"
-                        alt='Tenzies'
-                    />
-                </div>
-                <div className={`work-info ${inconsolata.className}`}>
-                    <h4 className={`text-[1.7rem] text-[#ff8303] font-black pt-1`}>Tenzies</h4>
-                    <h5 className={`text-[1.2rem] text-center pt-5`}>HTML5 - CSS - TypeScript - React</h5>
-                </div>
-                    <button aria-label="See more" type="button" onClick={ showModal }>See more &rarr;</button> 
-      </div>
-      <div className={clsx(`flex flex-col justify-center items-center transition-opacity ${inconsolata.className} bg-[#00000077] w-screen h-screen fixed top-0 z-[999]`,
+        {projects.map((p, index) => {
+          return (
+            <div key = {index} className="work-card z-[1]">
+              <div className="container-project">
+                  <Image 
+                      width="300"
+                      height="300"
+                      src={p.img}
+                      alt={p.title}
+                  />
+              </div>
+              <div className={`work-info ${inconsolata.className}`}>
+                  <h4 className={`text-[1.7rem] text-[#ff8303] font-black pt-1`}>{p.title}</h4>
+                  <h5 className={`text-[1.2rem] text-center pt-5`}>{p.tech}</h5>
+              </div>
+                  <button aria-label="See more" type="button" onClick={ () => showModal(p.id) }>See more &rarr;</button> 
+            </div>                 
+          )
+        }) }
+   
+      <div className={clsx(`flex flex-col justify-center items-center transition-opacity ${inconsolata.className} bg-[#00000077] w-screen h-screen fixed top-0 z-[900]`,
                             {
-                                'opacity-100': viewModal,
-                                'opacity-0': !viewModal,
-                                'pointer-events-auto': viewModal,
-                                'pointer-events-none': !viewModal    
+                                'opacity-100': viewModal.view,
+                                'opacity-0': !viewModal.view,
+                                'pointer-events-auto': viewModal.view,
+                                'pointer-events-none': !viewModal.view    
                             })}>
-          <div className={`bg-[#1b1a17] w-[90%] h-[700px] relative rounded-lg`}>
-          <svg className={`absolute top-o right-0 fill-[#ff8303]`} onClick={ showModal } xmlns="http://www.w3.org/2000/svg" height="48" width="48" viewBox="0 96 960 960"><path d="m338 768 142-142 142 142 50-50-142-142 142-142-50-50-142 142-142-142-50 50 142 142-142 142 50 50Zm142.138 233q-88.138 0-165.625-33.084-77.488-33.083-135.417-91.012T88.084 741.625Q55 664.276 55 576.138 55 487 88.084 409.513q33.083-77.488 90.855-134.969 57.772-57.482 135.195-91.013Q391.557 150 479.779 150q89.221 0 166.827 33.454 77.605 33.453 135.012 90.802 57.407 57.349 90.895 134.877Q906 486.66 906 576q0 88.276-33.531 165.747-33.531 77.471-91.013 135.278-57.481 57.808-134.831 90.891Q569.276 1001 480.138 1001Z"/></svg>
-              <div className={`flex flex-col justify-start items-center w-full h-full pt-4`}>
+          <div className={`bg-[#1b1a17] w-[90%] h-[670px] md:w-[90%] md:max-w-[900px] md:h-[360px] relative rounded-lg overflow-y-scroll md:overflow-y-hidden`}>
+          <svg onClick={ closeModal } className={`absolute top-o right-0 fill-[#ff8303] z-[999]`} xmlns="http://www.w3.org/2000/svg" height="48" width="48" viewBox="0 96 960 960"><path d="m338 768 142-142 142 142 50-50-142-142 142-142-50-50-142 142-142-142-50 50 142 142-142 142 50 50Zm142.138 233q-88.138 0-165.625-33.084-77.488-33.083-135.417-91.012T88.084 741.625Q55 664.276 55 576.138 55 487 88.084 409.513q33.083-77.488 90.855-134.969 57.772-57.482 135.195-91.013Q391.557 150 479.779 150q89.221 0 166.827 33.454 77.605 33.453 135.012 90.802 57.407 57.349 90.895 134.877Q906 486.66 906 576q0 88.276-33.531 165.747-33.531 77.471-91.013 135.278-57.481 57.808-134.831 90.891Q569.276 1001 480.138 1001Z"/></svg>
+            
+                <div className={`flex flex-col justify-start items-center md:flex-row md:justify-center w-full h-full pt-9 md:pt-0`}>
                 <Image 
                   width="300"
                   height="300"
-                  src="/Tenzies.webp"
-                  alt='Tenzies'
+                  src={ projects[viewModal.id].img }
+                  alt={ projects[viewModal.id].title  }
+                  className='md:pl-4'
                 />
                 <div className={`flex flex-col justify-center items-center pt-4`}>
-                  <h4 className={`text-[1.7rem] text-[#ff8303] text-center font-black pt-1`}>Tenzies</h4>
-                  <h5 className={`text-[1.2rem] text-center pt-5 pb-5`}>HTML5 - CSS - TypeScript - React</h5>
+                  <h4 className={`text-[1.7rem] text-[#ff8303] text-center font-black pt-1`}>{ projects[viewModal.id].title  }</h4>
+                  <h5 className={`text-[1.2rem] text-center pt-5 pb-5 w-[90%]`}>{ projects[viewModal.id].tech  }</h5>
                   <div className="line2 text-center"></div>
-                  <p className={`text-[1.2rem] text-center pt-5 w-[90%]`}>Let's play Tenzies! Roll until all dice are the same. Click each die to freeze it at its current value between rolls</p>
-                  <p className={`text-[1.2rem] text-[#ff8303] text-center font-black pt-5 drop-shadow`}><Link href="https://ornate-llama-98e056.netlify.app/" >Take a look</Link></p>
+                  <p className={`text-[1.2rem] text-center pt-5 w-[90%]`}>{ projects[viewModal.id].description  }</p>
+                  <p className={`text-[1.2rem] text-[#ff8303] text-center font-black pt-2 md:pt-5 drop-shadow`}><Link href= { projects[viewModal.id].link  }  >Take a look</Link></p>
               </div>
               </div>
-          </div>
-          
+          </div>         
       </div>
-      {/* <div className={ clsx(`modal-container z-[999]`,
-                            {
-                                'opacity-100': viewModal,
-                                'opacity-0': !viewModal,
-                                'pointer-events-auto': viewModal   
-                                
-                            }) }>
-        <div className="modal">
-            <svg className={`z-[999]`} onClick={ showModal } xmlns="http://www.w3.org/2000/svg" height="48" width="48" viewBox="0 96 960 960"><path d="m338 768 142-142 142 142 50-50-142-142 142-142-50-50-142 142-142-142-50 50 142 142-142 142 50 50Zm142.138 233q-88.138 0-165.625-33.084-77.488-33.083-135.417-91.012T88.084 741.625Q55 664.276 55 576.138 55 487 88.084 409.513q33.083-77.488 90.855-134.969 57.772-57.482 135.195-91.013Q391.557 150 479.779 150q89.221 0 166.827 33.454 77.605 33.453 135.012 90.802 57.407 57.349 90.895 134.877Q906 486.66 906 576q0 88.276-33.531 165.747-33.531 77.471-91.013 135.278-57.481 57.808-134.831 90.891Q569.276 1001 480.138 1001Z"/></svg>
-            <div className="container-project-m">
-              <Image 
-                width="300"
-                height="300"
-                src="/Tenzies.webp"
-                alt='Tenzies'
-              />
-            </div>
-            <div>
-              <h4>Algo</h4>
-              <h5>Algo más</h5>
-              <div className="line2"></div>
-              <p>yadayadayada</p>
-              <a></a>
-            </div>
-        </div>
-      </div> */}
-        <div className="work-card">
-                <div className="container-project">
-                    <Image 
-                        width="300"
-                        height="300"
-                        src="/spaceTravelers.webp"
-                        alt='Space Travelers'
-                    />
-                </div>
-                <div className={`work-info ${inconsolata.className}`}>
-                    <h4 className={`text-[1.7rem] text-[#ff8303] font-black pt-1`}>Space Travelers</h4>
-                    <h5 className={`text-[1.2rem] text-center pt-5`}>HTML5 - CSS - TypeScript - React - Redux - Vitest</h5>
-                </div>
-                    <button aria-label="See more" type="button">See more &rarr;</button> 
-        </div>
-
-        <div className="work-card">
-                <div className="container-project">
-                    <Image 
-                        width="300"
-                        height="300"
-                        src="/covid.webp"
-                        alt='Covid Metrics'
-                    />
-                </div>
-                <div className={`work-info ${inconsolata.className}`}>
-                    <h4 className={`text-[1.7rem] text-[#ff8303] font-black pt-1`}>Covid Metrics</h4>
-                    <h5 className={`text-[1.2rem] text-center pt-5`}>HTML5 - CSS - TypeScript - React - Redux - Jest</h5>
-                </div>
-                    <button aria-label="See more" type="button">See more &rarr;</button> 
-        </div>
-        <div className="work-card">
-                <div className="container-project">
-                    <Image 
-                        width="300"
-                        height="300"
-                        src="/mobileSecure.webp"
-                        alt='Mobile Secure'
-                    />
-                </div>
-                <div className={`work-info ${inconsolata.className}`}>
-                    <h4 className={`text-[1.7rem] text-[#ff8303] font-black pt-1`}>Mobile Secure</h4>
-                    <h5 className={`text-[1.2rem] text-center pt-5`}>HTML5 – CSS - TypeScript - React - Redux - NEXT.JS - Tailwind CSS</h5>
-                </div>
-                    <button aria-label="See more" type="button">See more &rarr;</button> 
-        </div>
       </section>
     </main>
   )
